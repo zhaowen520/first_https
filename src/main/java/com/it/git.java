0837c55f1df9aa5709fe3725014bb6bca960fb19
zhaowen <1273023448@qq.com>
2020-03-16 16:53:37 +0800

    修改了git.class

---
+++ src/main/java/com/it/git.java
@@ -0,0 +1,10 @@
+package com.it;
+
+public class git {
+
+    public static void main(String[] args) {
+        System.out.println("git demo");
+        System.out.println("oooooooooooo");
+    }
+
+}
